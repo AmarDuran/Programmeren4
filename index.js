8fe383139705d6123455613ce75919e7b4e80f5b
AmarDuran <amarduran2002@gmail.com>
2023-05-23 08:57:53 +0200

    Controllers toegevoegd

--- index.js
+++ index.js
@@ -20,137 +20,142 @@ function validatePhoneNumber(phoneNumber) {
 }
 
 app.use('/api/user', userRoutes);
-
-//UC-201 Regristreren als nieuwe user
-app.post('/api/user', (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).send('Naam, e-mail en wachtwoord zijn verplicht');
-  }
-  if (database.users.find(user => user.email === email)) {
-    return res.status(403).send({
-      success: false,
-      message: `Dit e-mailadres is al ingebruik`
-    });
-  }
-  if (!validateEmail(email)) {
-    return res.status(400).send({
-      success: false,
-      message: `Het opgegeven e-mailadres is ongeldig`
-    });
-  }
-  const idNumber = database.users.length + 1;
-  const newUser = { id: idNumber, name, email, password };
-  database.users.push(newUser);
-  res.status(201).json(newUser);
-});
-
-//UC-202 Opvragen overzicht van users
-// app.get('/api/user', (req, res) => {
-
-//   const searchCriteria = req.query.search || {};
+app.get('/api/user/profile', userController.getUserProfile);
+app.put('/api/user/:userId', userController.updateUser);
+app.delete('/api/user/:userId', userController.deleteUser);
 
 
-//   if (Object.keys(searchCriteria).length > 0) {
 
-//     const filteredUsers = database.users.filter(user => {
-//       return Object.keys(searchCriteria).every(key => {
-//         return user[key] === searchCriteria[key];
-//       });
-//     });
-//     return res.status(200).json(filteredUsers);
-//   } else {
-
-//     let usersToReturn = database.users;
-
-//     if (req.query.active !== undefined) {
-//       usersToReturn = database.users.filter(user => user.active === (req.query.active === 'true'));
-//     }
-
-//     return res.status(200).json(usersToReturn);
+// //UC-201 Regristreren als nieuwe user
+// app.post('/api/user', (req, res) => {
+//   const { name, email, password } = req.body;
+//   if (!name || !email || !password) {
+//     return res.status(400).send('Naam, e-mail en wachtwoord zijn verplicht');
 //   }
+//   if (database.users.find(user => user.email === email)) {
+//     return res.status(403).send({
+//       success: false,
+//       message: `Dit e-mailadres is al ingebruik`
+//     });
+//   }
+//   if (!validateEmail(email)) {
+//     return res.status(400).send({
+//       success: false,
+//       message: `Het opgegeven e-mailadres is ongeldig`
+//     });
+//   }
+//   const idNumber = database.users.length + 1;
+//   const newUser = { id: idNumber, name, email, password };
+//   database.users.push(newUser);
+//   res.status(201).json(newUser);
 // });
 
-//UC-203 Opvragen van gebruikersprofiel
-app.get('/api/user/profile', (req, res) => {
-  const user = database.users.find(user => user.id === 1);
-  if (!user) {
-    // Gebruiker niet gevonden
-    return res.status(404).json({ error: 'Gebruiker niet gevonden' });
-  }
-  // Stuur het profiel terug naar de gebruiker
-  return res.status(200).json(user);
-});
+// //UC-202 Opvragen overzicht van users
+// // app.get('/api/user', (req, res) => {
 
-// UC-204 Opvragen van usergegevens bij ID (Misschien nog even aanpassen)
-app.get('/api/user/:userId', (req, res) => {
-  if (!database) {
-    return res.status(500).send('Database not defined');
-  }
-  const userId = Number(req.params.userId); // convert to number type
-  const user = database.users.find(user => user.id === userId);
+// //   const searchCriteria = req.query.search || {};
 
-  if (!user) {
-    return res.status(404).send('User not found');
-  }
 
-  const meals = database.meals.filter(meal => meal.cook === user.firstname);
+// //   if (Object.keys(searchCriteria).length > 0) {
 
-  res.json({
-    user,
-    meals
-  });
-});
+// //     const filteredUsers = database.users.filter(user => {
+// //       return Object.keys(searchCriteria).every(key => {
+// //         return user[key] === searchCriteria[key];
+// //       });
+// //     });
+// //     return res.status(200).json(filteredUsers);
+// //   } else {
 
-// UC-205 Updaten van usergegevens
-app.put('/api/user/:userId', (req, res) => {
-  const userId = 1; 
-  const user = database.users.find(user => user.id === 1);
+// //     let usersToReturn = database.users;
 
-  if (!user) {
-    return res.status(404).send('User not found');
-  }
+// //     if (req.query.active !== undefined) {
+// //       usersToReturn = database.users.filter(user => user.active === (req.query.active === 'true'));
+// //     }
 
-  if (user.id !== 1) { 
-    return res.status(403).send('Forbidden');
-  }
-  const { email, firstname, lastname, phoneNumber, street, city } = req.body;
+// //     return res.status(200).json(usersToReturn);
+// //   }
+// // });
 
-  if (!email) {
-    return res.status(400).send('Email is required');
-  }
-  if (!validatePhoneNumber(phoneNumber)) { 
-    return res.status(400).send('Invalid phone number');
-  }
-  // Update user data
-  user.email = email;
-  user.firstname = firstname || user.firstname;
-  user.lastname = lastname || user.lastname;
-  user.phoneNumber = phoneNumber;
-  user.street = street || user.street;
-  user.city = city || user.city;
+// //UC-203 Opvragen van gebruikersprofiel
+// app.get('/api/user/profile', (req, res) => {
+//   const user = database.users.find(user => user.id === 1);
+//   if (!user) {
+//     // Gebruiker niet gevonden
+//     return res.status(404).json({ error: 'Gebruiker niet gevonden' });
+//   }
+//   // Stuur het profiel terug naar de gebruiker
+//   return res.status(200).json(user);
+// });
 
-  res.json(user);
-});
+// // UC-204 Opvragen van usergegevens bij ID (Misschien nog even aanpassen)
+// app.get('/api/user/:userId', (req, res) => {
+//   if (!database) {
+//     return res.status(500).send('Database not defined');
+//   }
+//   const userId = Number(req.params.userId); // convert to number type
+//   const user = database.users.find(user => user.id === userId);
 
-// UC-206 Verwijderen van user
-app.delete('/api/user/:userId', (req, res) => {
-  const userId = 1;
-  const userIndex = database.users.findIndex(user => user.id === userId);
-  const currentUser = {id: 1};
+//   if (!user) {
+//     return res.status(404).send('User not found');
+//   }
 
-  if (userIndex === -1) {
-    return res.status(404).send('User not found');
-  }
+//   const meals = database.meals.filter(meal => meal.cook === user.firstname);
 
-  if (currentUser.id !== userId) {
-    return res.status(403).send('Unauthorized');
-  }
+//   res.json({
+//     user,
+//     meals
+//   });
+// });
 
-  database.users.splice(userIndex, 1);
+// // UC-205 Updaten van usergegevens
+// app.put('/api/user/:userId', (req, res) => {
+//   const userId = 1; 
+//   const user = database.users.find(user => user.id === 1);
 
-  res.status(200).json({ message: 'User deleted successfully' });
-});
+//   if (!user) {
+//     return res.status(404).send('User not found');
+//   }
+
+//   if (user.id !== 1) { 
+//     return res.status(403).send('Forbidden');
+//   }
+//   const { email, firstname, lastname, phoneNumber, street, city } = req.body;
+
+//   if (!email) {
+//     return res.status(400).send('Email is required');
+//   }
+//   if (!validatePhoneNumber(phoneNumber)) { 
+//     return res.status(400).send('Invalid phone number');
+//   }
+//   // Update user data
+//   user.email = email;
+//   user.firstname = firstname || user.firstname;
+//   user.lastname = lastname || user.lastname;
+//   user.phoneNumber = phoneNumber;
+//   user.street = street || user.street;
+//   user.city = city || user.city;
+
+//   res.json(user);
+// });
+
+// // UC-206 Verwijderen van user
+// app.delete('/api/user/:userId', (req, res) => {
+//   const userId = 1;
+//   const userIndex = database.users.findIndex(user => user.id === userId);
+//   const currentUser = {id: 1};
+
+//   if (userIndex === -1) {
+//     return res.status(404).send('User not found');
+//   }
+
+//   if (currentUser.id !== userId) {
+//     return res.status(403).send('Unauthorized');
+//   }
+
+//   database.users.splice(userIndex, 1);
+
+//   res.status(200).json({ message: 'User deleted successfully' });
+// });
 
 
 app.listen(port, () => {
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -46,6 +46,53 @@ const userController = {
       }
     });
   },
+  createUser: (req, res) => {
+    let newUser = req.body;
+    let { firstName, lastName, emailAddress } = req.body;
+
+    try {
+      assert(typeof firstName === 'string', 'firstName must be string')
+      assert(typeof lastName === 'string', 'lastName must be string')
+      assert(typeof emailAddress === 'string', 'emailAddress must be string')
+      index = database.index + 1;
+      newUser = {
+        id: index,
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: emailAddress,
+      };
+      database.users.push(newUser);
+      res.status(201).json({
+        status: 201,
+        message: `User met ID ${index} is toegevoegd`,
+        data: newUser,
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 400,
+        message: err.toString(),
+        data: {},
+      })
+    }
+  },
+
+  getUser: (req, res) => {
+    let userId = req.params.userId;
+    let user = database.users.filter((item) => item.id == userId);
+    if (user[0]) {
+      res.status(200).json({
+        status: 200,
+        message: `User met ID ${userId} is gevonden`,
+        data: user[0],
+      })
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: `User met ID ${userId} niet gevonden`,
+        data: {},
+      });
+    }
+  },
 
   getUserProfile: (req, res, next) => {
     req.userId = 1;
@@ -130,8 +177,137 @@ const userController = {
       data: user
     });
   },
+  updateUser: (req, res) => {
+    try {
+      const { emailaddress, firstName, lastName } = req.body;
+      const userIndex = database.users.findIndex(
+        (user) => user.emailaddress === emailaddress
+      );
 
-  deleteUser: (req, res) => { }
+      if (userIndex === -1) {
+        throw new Error('Gebruiker niet gevonden');
+      }
+
+      const { firstname, lastname } = updateData;
+
+      if (firstname && !firstname.trim()) {
+        throw new Error('Voornaam is verplicht');
+      }
+
+      if (lastname && !lastname.trim()) {
+        throw new Error('Achternaam is verplicht');
+      }
+
+      // Werk de gebruikersgegevens bij met de opgegeven updateData
+
+      if (firstname) user.firstname = firstname;
+      if (lastname) user.lastname = lastname;
+
+      // Sla de bijgewerkte gebruiker op in de database
+      database.users[userIndex] = user;
+      res.status(200).json({
+        status: 200,
+        message: 'Gebruiker is met succes bijgewerkt',
+        data: user,
+      });
+    } catch (err) {
+      let statusCode = 400;
+      if (err.message === 'Gebruiker niet gevonden') {
+        statusCode = 404;
+      }
+      res.status(statusCode).json({
+        status: statusCode,
+        message: err.message.toString(),
+        data: {},
+      });
+    }
+  },
+
+  deleteUser: (req, res) => {
+    let userId = req.params.userId;
+    let user = database.users.filter((item) => item.id == userId);
+
+    if (user[0]) {
+      database.users.splice(user.id - 1);
+      res.status(200).json({
+        status: 200,
+        message: `User met ID ${userId} is verwijderd`,
+        data: {},
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: 'Gebruiker bestaat niet',
+        data: {},
+      });
+    };
+  },
+
+  updateMeal: (req, res, next) => {
+    const { mealId, token, newData } = req.body;
+    logger.trace('Updating meal data for meal', mealId);
+    // Step 1: Validate user token
+    if (!token) {
+      return next({
+        code: 401,
+        message: 'Unauthorized'
+      });
+    }
+
+    // Step 2: Validate incoming data
+    if (!newData || Object.keys(newData).length === 0) {
+      return next({
+        code: 400,
+        message: 'Invalid data'
+      });
+    }
+
+    // Step 3: Find the meal in the system
+    Meal.findById(mealId, (err, meal) => {
+      if (err) {
+        logger.error(err.message);
+        return next({
+          code: 500,
+          message: err.message
+        });
+      }
+
+      if (!meal) {
+        return next({
+          code: 404,
+          message: 'Meal not found'
+        });
+      }
+
+      // Step 4: Validate user ownership
+      if (meal.userId !== req.userId) {
+        return next({
+          code: 403,
+          message: 'User does not own the meal'
+        });
+      }
+
+      // Step 5: Update meal data
+      meal.update(newData, (err, updatedMeal) => {
+        if (err) {
+          logger.error(err.message);
+          return next({
+            code: 500,
+            message: err.message
+          });
+        }
+
+        logger.trace('Meal data updated:', updatedMeal);
+
+        res.status(200).json({
+          code: 200,
+          message: 'Meal data updated',
+          data: updatedMeal
+        });
+      });
+    });
+  },
 };
+
 
 module.exports = userController;
